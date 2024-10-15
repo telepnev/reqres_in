e82c229f624e6b8abbe0dd2b67ccf57fe4c6b775
Telepnev Evgeny <telepnef@gmail.com>
2024-10-15 19:05:09 +0300

    UsersResponse

---
+++ src/main/java/model/UsersResponse.java
@@ -0,0 +1,51 @@
+package model;
+
+public class UsersResponse {
+
+    private Integer page;
+    private Integer perPage;
+    private Integer total;
+    private List<Data> data;
+    private Integer totalPage;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<Data> getData() {
+        return data;
+    }
+
+    public void setData(List<Data> data) {
+        this.data = data;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
+}
+
