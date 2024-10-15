0a6e21fb8542cebd690d4a61a34bf2d3de47d7f6
Evgeniy Telepnev <telepnef@yandex.ru>
2024-10-15 21:06:24 +0300

    created test testGetUsersList()

--- src/main/java/model/UsersResponse.java
+++ src/main/java/model/UsersResponse.java
@@ -1,12 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class UsersResponse {
 
     private Integer page;
-    private Integer perPage;
+    private Integer per_page;
     private Integer total;
-    private List<Data> data;
-    private Integer totalPage;
+    private List<UserData> data;
+    private Integer total_pages;
+    private Support support;
 
     public Integer getPage() {
         return page;
@@ -16,20 +19,12 @@ public class UsersResponse {
         this.page = page;
     }
 
-    public Integer getTotalPage() {
-        return totalPage;
+    public Integer getPer_page() {
+        return per_page;
     }
 
-    public void setTotalPage(Integer totalPage) {
-        this.totalPage = totalPage;
-    }
-
-    public List<Data> getData() {
-        return data;
-    }
-
-    public void setData(List<Data> data) {
-        this.data = data;
+    public void setPer_page(Integer per_page) {
+        this.per_page = per_page;
     }
 
     public Integer getTotal() {
@@ -40,12 +35,30 @@ public class UsersResponse {
         this.total = total;
     }
 
-    public Integer getPerPage() {
-        return perPage;
+    public List<UserData> getData() {
+        return data;
     }
 
-    public void setPerPage(Integer perPage) {
-        this.perPage = perPage;
+    public void setData(List<UserData> data) {
+        this.data = data;
+    }
+
+    public Integer getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(Integer total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public Support getSupport() {
+        return support;
+    }
+
+    public void setSupport(Support support) {
+        this.support = support;
     }
 }
+
+
 
---
+++ src/test/java/tests/GetListUsersTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import model.UserData;
+import model.UsersResponse;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GetListUsersTests {
+
+    private final String BASE_URL = "https://reqres.in/api/users";
+    // ObjectMapper из библиотеки com.fasterxml.jackson.databind, мапит JSON с нашими классами, тобишь с обьектами
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testGetUsersList() throws Exception {
+        // зарос Get
+        Response response = RestAssured
+                .given()
+                .when()
+                // тут хедары и всякая остальная инфа
+                .get(BASE_URL)
+                .then()
+                // ответ
+                .statusCode(200)
+                .extract().response();
+
+        // Десериализация Json - ответа в обьект UsersResponse
+        UsersResponse usersResponse = objectMapper.readValue(response.asString(), UsersResponse.class);
+
+        //Проверки
+        // "per_page": 6
+        assertEquals(6, usersResponse.getData().size(), "Кол-во пользователей не совпадает");
+        //"total_pages": 2
+        assertEquals(2, usersResponse.getTotal_pages(), "Кол-во страниц не совпадает");
+        // "page": 2
+        assertEquals(1, usersResponse.getPage(),"Кол-во страниц не совпадает");
+        // "total": 12,
+        assertEquals(12, usersResponse.getTotal(), "Кол-во всех юзеров не совпадает");
+
+
+        //  проходим циклом и проверяем поля у юзеров
+        for(UserData user : usersResponse.getData()) {
+
+            // Проверяем что почта пользователя заканчивается на @reqres.in
+            assertTrue(user.getEmail().endsWith("@reqres.in"),
+                    "Email пользователя не совпадает, Email = " + user.getEmail());
+
+            // Проверяем ID юзера
+            assertNotNull(user.getId(), "ID юзера вернуло Null");
+
+           // загрузки аватара image.jpg
+            assertTrue(user.getAvatar().endsWith("image.jpg"), "Ошибка загрузки аватара");
+        }
+    }
+}
